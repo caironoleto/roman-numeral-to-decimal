e2fdcda62c2eb0d84a23f0b20b5d4ca4701c8393
Cairo Noleto <caironoleto@gmail.com>
2011-05-06 15:41:47 -0300

    converting several cases

--- lib/romain.rb
+++ lib/romain.rb
@@ -1,6 +1,22 @@
 class Romain < String
   NUMERALS = {"I" => 1, "V" => 5, "X" => 10, "L" => 50, "C" => 100, "D" => 500, "M" => 1000}
+
   def to_decimal
-    NUMERALS[self]
+    number = 0
+    roman_number = self
+    if roman_number =~ /\(/
+      roman_number.gsub!("(", "")
+      roman_number.gsub!(")", "")
+      by_1000 = true
+    end
+    roman_number.size.times do |position|
+      if NUMERALS[roman_number[position + 1]] && NUMERALS[roman_number[position]] < NUMERALS[roman_number[position + 1]]
+        number -= NUMERALS[roman_number[position]]
+      else
+        number += NUMERALS[roman_number[position]]
+      end
+    end
+    number *= 1000 if by_1000
+    number
   end
 end
