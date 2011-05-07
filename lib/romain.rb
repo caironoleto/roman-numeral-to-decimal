a0fcb792e075a25c6e731e61a1497a10fa689cdb
Cairo Noleto <caironoleto@gmail.com>
2011-05-07 13:51:28 -0300

    all works!

--- lib/romain.rb
+++ lib/romain.rb
@@ -3,20 +3,45 @@ class Romain < String
 
   def to_decimal
     number = 0
-    roman_number = self
-    if roman_number =~ /\(/
-      roman_number.gsub!("(", "")
-      roman_number.gsub!(")", "")
-      by_1000 = true
-    end
-    roman_number.size.times do |position|
-      if NUMERALS[roman_number[position + 1]] && NUMERALS[roman_number[position]] < NUMERALS[roman_number[position + 1]]
-        number -= NUMERALS[roman_number[position]]
-      else
-        number += NUMERALS[roman_number[position]]
-      end
-    end
-    number *= 1000 if by_1000
+    roman_numerals.each {|numeral, factor| number += calculate_number(numeral, factor) }
     number
   end
+
+  private
+  def thousands?(number = self)
+    number =~ /\(/
+  end
+
+  def million?(number = self)
+    number =~ /\(\(/
+  end
+
+  def roman_numerals
+    numbers = self.split("\)") if thousands? or million?
+    numbers ||= [self]
+    numbers = numbers.map do |number|
+      [number.gsub("\(", ""), calculate_factor(number)]
+    end
+    numbers
+  end
+
+  def calculate_factor(number)
+    factor = 1 if !million?(number) and !thousands?(number)
+    factor = 1000 if thousands?(number)
+    factor = 1000000 if million?(number)
+    factor
+  end
+
+  def calculate_number(numeral, factor)
+    number = 0
+    numeral.size.times do |position|
+      if NUMERALS[numeral[position + 1]] && NUMERALS[numeral[position]] < NUMERALS[numeral[position + 1]]
+        number -= NUMERALS[numeral[position]]
+      else
+        number += NUMERALS[numeral[position]]
+      end
+    end
+    number *= factor
+    number
+  end    
 end
--- spec/romain_spec.rb
+++ spec/romain_spec.rb
@@ -16,16 +16,24 @@ describe Romain do
   it { Romain.new("L").to_decimal.should == 50 }
   it { Romain.new("C").to_decimal.should == 100 }
   it { Romain.new("D").to_decimal.should == 500 }
+  it { Romain.new("DCCCLXXXVIII").to_decimal.should == 888 }
+  it { Romain.new("CM").to_decimal.should == 900 }
   it { Romain.new("M").to_decimal.should == 1000 }
   it { Romain.new("(V)").to_decimal.should == 5000 }
   it { Romain.new("(VI)").to_decimal.should == 6000 }
   it { Romain.new("(VII)").to_decimal.should == 7000 }
   it { Romain.new("(VIII)").to_decimal.should == 8000 }
+  it { Romain.new("(VIII)DCCCLXXXVIII").to_decimal.should == 8888 }
   it { Romain.new("(X)").to_decimal.should == 10000 }
   it { Romain.new("(L)").to_decimal.should == 50000 }
+  it { Romain.new("(LXXX)").to_decimal.should == 80000 }
   it { Romain.new("(C)").to_decimal.should == 100000 }
   it { Romain.new("(D)").to_decimal.should == 500000 }
-  it { Romain.new("(DCCC)").to_decimal.should == 500000 }
+  it { Romain.new("(DCCC)").to_decimal.should == 800000 }
   it { Romain.new("(M)").to_decimal.should == 1000000 }
-  it { Romain.new("((VIII))(DCCC)(LXXX)(VIII)DCCCLXXXVIII").to_decimal.should == 8888888 }
+  it { Romain.new("((VIII))").to_decimal.should == 8000000}
+  it "should convert to 8888888" do
+    romain_number = Romain.new("((VIII))(DCCC)(LXXX)(VIII)DCCCLXXXVIII")
+    romain_number.to_decimal.should == 8888888
+  end
 end
